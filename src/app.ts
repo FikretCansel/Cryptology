9bcdc020face1045f3ac19c82836504d659ddcce
FikretCansel <fikretcansel25@hotmail.com>
2022-11-30 18:09:53 +0300

    Version 3- Image Encryption

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import path from "path";
 import Message from "./models/chatGroups/Message";
 import { ObjectId, WithId } from "mongodb";
 import CryptoJS from "crypto-js";
+import fs from "fs";
 
 
 var bodyParser = require('body-parser');
@@ -68,6 +69,43 @@ app.post('/users/login',(req:Request,res:Response,next:NextFunction)=> {
   if(username==="fikret" && password==="fikret"){
     res.redirect(`/chat?userName=${username}`);
   }else res.redirect("/?error=true");
+});
+
+app.post('/encrypt-image',async(req:Request,res:Response,next:NextFunction)=> {
+  let userName="fikret";
+  fs.readFile('image.jpg', async function(err, data) {
+  if (err) throw err // Fail if the file can't be read.
+  console.log(data);
+  let stringData = data.toString("base64");
+  const defaultBuf = Buffer.from(stringData,"base64");
+  console.log("defaultBuf",defaultBuf)
+
+  const keyDoc = await getSecretKey();
+  console.log("Key",keyDoc)
+  var ciphertext = CryptoJS.AES.encrypt(stringData, keyDoc?.key).toString();
+  var base64ParseCipherWord = CryptoJS.enc.Base64.parse(ciphertext);
+  const base64ParseCipherText = CryptoJS.enc.Base64.stringify(base64ParseCipherWord);
+  const buf = Buffer.from(base64ParseCipherText,"base64");
+  console.log("Şifreli buf",buf)
+  http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'image/jpeg'})
+    res.end(buf2) // Send the file data to the browser.
+  }).listen(3001)
+  var decryptedText = CryptoJS.AES.decrypt(ciphertext, keyDoc?.key).toString();
+  
+  var base64ParseDeCryptedWord = CryptoJS.enc.Base64.parse(decryptedText);
+  const base64ParseDeCryptedText = CryptoJS.enc.Base64.stringify(base64ParseDeCryptedWord);
+
+  const buf2 = Buffer.from(base64ParseDeCryptedText,"base64");
+  console.log("decryptedBuf",buf2)
+
+  http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'image/jpeg'})
+    res.end(buf2) // Send the file data to the browser.
+  }).listen(3002)
+
+})
+  res.redirect(`/chat?userName=${userName}`);
 });
 
 app.post('/chat/add',async (req:Request,res:Response,next:NextFunction)=> {
